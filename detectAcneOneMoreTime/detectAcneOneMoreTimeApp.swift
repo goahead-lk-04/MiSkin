e8fe5e7dcca3e4daba00be511a6481b1ddaff0a1
Lisa <kuchynaliza86@gmail.com>
2024-01-09 05:27:43 +0200

    Initial Commit

---
+++ detectAcneOneMoreTime/detectAcneOneMoreTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  detectAcneOneMoreTimeApp.swift
+//  detectAcneOneMoreTime
+//
+//  Created by Lisa Kuchyna on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct detectAcneOneMoreTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
